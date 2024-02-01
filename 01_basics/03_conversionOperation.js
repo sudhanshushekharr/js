80969ddffa0c1eacb92bd4fbbb22b7ff8296733a
sshekhar <113382768+sudhanshushekharr@users.noreply.github.com>
2024-02-01 10:56:41 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score="hitesh"
+console.log(typeof score)
+console.log(typeof(score))
+
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+//"33"=>33 when converted into number type vice versa is also true
+//"33abc"=>NaN when converted into number type
+//true=>1;false=>0 when converted into number type
+//null=>0 when converted into number type
+//undefined=>NaN when converted into number type
+//"string"=>Nan when converted into number type
+
+
+let isLoggedIn="shekhar"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=>true
+//""=>false
+//"shekhar"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
