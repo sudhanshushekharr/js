1c9fd48364f92f7abbd91899df5b2d869ee7b4cf
sshekhar <113382768+sudhanshushekharr@users.noreply.github.com>
2024-02-01 13:17:26 +0000

    datattypes summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -4,36 +4,36 @@
 
 //javascript is a dynamically typed language
 
-const score = 100
-const scoreValue=100.3
+const score = 100 //number
+const scoreValue=100.3 //number
 
-const isLoggedIn=false
-const outsideTemp=null
-let userEmail;//undefined
+const isLoggedIn=false //boolean
+const outsideTemp=null //object
+let userEmail;//undefined 
 
 const id=Symbol('123')
 const anotherId=Symbol('123')
-console.log(id==anotherId);
+console.log(id==anotherId); //false as Symbol is used for unique things
 
 
-const bigNumber=34567894512564585545n
-console.log(typeof bigNumber);
+const bigNumber=34567894512564585545n //bigint
+console.log(typeof userEmail);
 
 //Reference(Non primitive)
 
 //Array, Objects, Functions
 
-const heros =["shaktiman","naagraj","doga"]
+const heros =["shaktiman","naagraj","doga"] //object
 
 let myObj={
     name:"hitesh",
     age:22,
 
 
-}
+} //Object
 
 const myFunction=function(){
     console.log("Hello World");
-}
+} //function
 
 console.log(typeof myFunction)
